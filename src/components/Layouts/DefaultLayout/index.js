eb2401eca62efc667aaead7d39f0a1df60cee287
hoanghuu2602 <hoanghuu2602@gmail.com>
2023-10-25 17:56:22 +0700

    upp

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,14 +1,17 @@
 import Header from '../componentsLayout/Header'
 import Sidebar from '../componentsLayout/Sidebar'
 import styles from './DefaultLayout.module.scss'
-import clsx from 'clsx';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles)
+
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className={clsx(styles.container)}>
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className='Content'> {children}</div>
+                <div className={cx('content')}> {children}</div>
             </div>
         </div>
     );
--- src/components/Layouts/componentsLayout/Header/index.js
+++ src/components/Layouts/componentsLayout/Header/index.js
@@ -1,12 +1,33 @@
-import clsx from "clsx";
+// import clsx from "clsx";
+import classNames from "classnames/bind";
 import styles from './Header.module.scss'
+import images from "../../../../assets/images";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass, faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-
+const cx = classNames.bind(styles)
 function Header() {
-    return (<div className={clsx(styles.wraperHeader)} >
-        <div className={clsx(styles.header)} >
-
-            <h1>Header</h1>
+    return (
+        <div className={cx('wrapper')} >
+        {/* <div className={clsx(styles.wraperHeader)} > */}
+        <div className={cx('inner')} >
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Logo" />
+            </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck='false' />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                        {/* <FontAwesomeIcon icon="fa-solid fa-circle-xmark" /> */}
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon className ={cx('load')} icon={faSpinner} />
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                    
+            </div>
+                <div className={cx('actions')}>actions</div>
         </div>
     </div>
 
