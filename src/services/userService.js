6c5e1f323ba2d844bbbdd325d44414cc5bcea011
hvhuu2602 <hvhuu2602@gmail.com>
2023-11-29 23:36:42 +0700

    sfsdrr

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const loginUser = (valueLogin, password) => {
     return axios.post('http://localhost:8080/api/v1/login', {
-        valueLogin, password
+        valueLogin, password,withCredential:true
     })
 }
 const fetchListUser = (page,limit) => {
